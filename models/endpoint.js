c92e606b6a7be590367d3702926653e8290105bc
John Mark Perocho <per.eight@gmail.com>
2016-11-30 18:16:29 +0800

    Endpoints run and test

--- models/endpoint.js
+++ models/endpoint.js
@@ -2,8 +2,21 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var endpoint = new Schema({
+var endpointSchema = new Schema({
+	id: { type: Number, required: true, default: 0},
 	endpoint: [Schema.Types.Mixed]
 }, {strict: false});
 
-module.exports = mongoose.model('endpoint', endpoint);
+var endpoint = mongoose.model('endpoint', endpointSchema);
+
+endpointSchema.pre('save', function(next) {
+    var doc = this;
+    endpoint.findOne({}, {}, {sort: {id:-1}}, function(error, id)   {
+        if(error)
+            return next(error);
+        console.log(this);
+        next();
+    });
+});
+
+module.exports = endpoint;
--- server.js
+++ server.js
@@ -1,2 +1,28 @@
 'use strict';
-var mongoose =
+var express = require('express');
+var mongoose = require('mongoose');
+
+var app = express();
+mongoose.connect('mongodb://localhost/mushapi');
+
+var Endpoint = require('./models/endpoint');
+
+app.get('/',function(req,res){
+	res.json('it works');
+})
+
+app.post('/',function(req,res){
+	var endpoint = new Endpoint({
+		endpoint: 'test'
+	})
+
+	endpoint.save(function(err,data){
+		if (err) console.log();
+
+		res.json(data);
+	});
+})
+
+
+
+app.listen(3000);
